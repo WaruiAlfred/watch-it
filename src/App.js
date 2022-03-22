dd1a6c314c68d4b824f2bc99d1ce58f2722ab95a
WaruiAlfred <alfredwarui17@gmail.com>
2022-03-23 01:06:31 +0300

    add overlay

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {Fragment} from "react";
+import { Fragment } from "react";
 import Navbar from "./components/ui/Navbar";
 
 import "./App.css";
 import AllMovies from "./components/movies/AllMovies";
 
 function App() {
-  return <Fragment>
-  <Navbar/>
-  <h1>WATCH IT</h1>
-  <AllMovies/>
-  </Fragment>;
+  return (
+    <Fragment>
+      <div className="app">
+        <div className="overlay"></div>
+        <Navbar />
+      </div>
+      <AllMovies />
+    </Fragment>
+  );
 }
 
 export default App;
